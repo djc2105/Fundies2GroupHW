4590f5b88471b86a61f5e6e631865b64dbd7a8e8
macegoalie14 <66329065+macegoalie14@users.noreply.github.com>
2020-10-14 20:05:46 -0400

    Mason Update 1.0
    - added functionality to PlayerFish class
    - commented out some code
    - removed herobrine

--- src/Fishy.java
+++ src/Fishy.java
@@ -1,6 +1,8 @@
+
 import tester.Tester;
 import javalib.funworld.*;
 import javalib.worldimages.*;
+import testClass.PlayerFish;
 import java.awt.Color;
 import java.util.Random;
 
@@ -11,56 +13,141 @@ import java.util.Random;
  * 
  */
 
-public class Fishy extends World{
-  int width = 800;
-  int height = 600;
-  Random rand;
-  WorldImage backGround = new RectangleImage(width, height, OutlineMode.SOLID, Color.blue);
+// contains the Fishy! game
+public class Fishy extends World {
   
+  // constants
+  int SCREEN_WIDTH = 800;
+  int SCREEN_HEIGHT = 600;
+  WorldImage BACKGROUND = 
+      new RectangleImage(SCREEN_WIDTH, SCREEN_HEIGHT, OutlineMode.SOLID, Color.blue);
+  
+  // variables 
+  PlayerFish player;
+  Random rand;
+
   // Constructor for playing
-  public Fishy() {
+  public Fishy(PlayerFish player) {
+    this.player = player;
     this.rand = new Random();
   }
-  
+
   // Constructor for use in testing
-  public Fishy(Random rand) {
+  public Fishy(PlayerFish player, Random rand) {
+    this.player = player;
     this.rand = rand;
   }
   
+  //moves the player when a key is pressed
+  public World onKeyEvent(String ke) {
+    return new Fishy(player.moveFish(ke));
+  }
+
+  // draws the Fishy! game
   public WorldScene makeScene() {
     return this
         .getEmptyScene()
-        .placeImageXY(this.backGround, 400, 300);
+        .placeImageXY(this.BACKGROUND, 400, 300);
   }
 }
-  
+
+// an interface to represent a fish in the Fishy! game
 interface IFish {
-  WorldImage draw(Color c);
+  
+  // constants
+  int SCALE = 10;
+  int SCREEN_WIDTH = 800;
+  int SCREEN_HEIGHT = 600;
+
+  // draws the IFish
+  WorldImage drawFish();
+  
 }
 
-abstract class AFish implements IFish{
+// an abstract class to represent a Fish in the Fishy! game
+abstract class AFish implements IFish {
+  
+  // variables
   int x;
   int y;
   int size;
-  
-  public WorldImage draw(Color c) { 
-    return new EllipseImage(this.size * 20, this.size * 10, OutlineMode.SOLID, c);
+  Color color;
+
+  // draws the AFish
+  public WorldImage drawFish() {
+    return new BesideImage(
+        new RotateImage(
+            new EquilateralTriangleImage(this.size * SCALE, 
+                OutlineMode.SOLID, this.color), 
+            90), 
+        new EllipseImage(this.size * SCALE * 2, 
+            this.size * SCALE, 
+            OutlineMode.SOLID, this.color));
   }
-  
+
 }
 
 
+// the fish controlled by the player in the Fishy! game
 class PlayerFish extends AFish {
-  
+
+  // constructor
+  public PlayerFish(int x, int y, int size, Color color) {
+    super.x = x;
+    super.y = y; 
+    super.size = size;
+    super.color = color;
+  }
+
+  // moves the fish based on the key input
+  // if the fish leaves the screen, return on the other side
+  public PlayerFish moveFish(String ke) {
+    if (ke.equals("left")) {
+      int newX = super.x - 5;
+      if (newX < 0) {
+        return new PlayerFish(SCREEN_WIDTH + newX, super.y, super.size, super.color);
+      } else {
+        return new PlayerFish(newX, super.y, super.size, super.color);
+      }
+    } else if (ke.equals("right")) {
+      int newX = super.x + 5;
+      if (newX > SCREEN_WIDTH) {
+        return new PlayerFish(newX - SCREEN_WIDTH, super.y, super.size, super.color);
+      } else {
+        return new PlayerFish(newX, super.y, super.size, super.color);
+      }
+    } else if (ke.equals("up")) {
+      int newY = super.y - 5;
+      if (newY < 0) {
+        return new PlayerFish(super.x, SCREEN_HEIGHT + newY, super.size, super.color);
+      } else {
+        return new PlayerFish(super.x, super.y, super.size, super.color);
+      }
+    } else if (ke.equals("down")) {
+      int newY = super.y + 5;
+      if (newY > SCREEN_HEIGHT) {
+        return new PlayerFish(super.x, newY - SCREEN_HEIGHT, super.size, super.color);
+      } else {
+        return new PlayerFish(super.x, super.y, super.size, super.color);
+      }
+    } else {
+      return this;
+    }
+  }
+
 }
 
+// the non-playable fish in the Fishy! game
 class BotFish extends AFish {
+  
+  // variables
   boolean movingRight;
   
+  // constructor
   public BotFish(Random rand, int height) {
     int randInt = rand.nextInt(height - 20) + 10; // Gets a y height that is between 10-(height-10)
     super.size = rand.nextInt(10) + 1; // Generates a size between 1-10
-    
+
     if (randInt < ((height-10)/2)) {
       movingRight = true;
       super.x = 0;
@@ -74,27 +161,36 @@ class BotFish extends AFish {
   }
 }
 
-interface ILoBot {
-  
-}
+// an interface to represent a list of BotFish
+interface ILoBot {}
 
+// a non-empty list of BotFish
 class ConsLoBot implements ILoBot {
+  
+  // variables
   BotFish first;
   ILoBot rest;
-  
+
+  // constructor
   public ConsLoBot(BotFish first, ILoBot rest) {
     this.first = first;
     this.rest = rest;
   }
-}
-
-class MtLoBot implements ILoBot {
   
 }
 
+// an empty list of BotFish
+class MtLoBot implements ILoBot {}
+
 class TestFishy {
+  
+  // example variables
+  PlayerFish player1 = new PlayerFish(400, 300, 1, Color.RED);
+  
+  // a class to test Fishy
   boolean testFishy(Tester t) {
-    Fishy w = new Fishy();
+    Fishy w = new Fishy(this.player1);
     return w.bigBang(800, 600, 1);
   }
+  
 }
