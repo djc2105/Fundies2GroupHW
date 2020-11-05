faeff730cdabcbea6ebbf7956692163ba9e52d0c
macegoalie14 <66329065+macegoalie14@users.noreply.github.com>
2020-11-04 19:45:07 -0500

    Mason Update 4.2
    - this code doesnt run

--- src/Deque.java
+++ src/Deque.java
@@ -1,4 +1,7 @@
 
+import java.util.function.*;
+import tester.Tester;
+
 // represents a doubly linked list
 class Deque<T> {
   
@@ -17,10 +20,44 @@ class Deque<T> {
   /* fields: 
    *   this.header ... Sentinel<T>
    * methods: 
-   * 
+   *   this.size() ... int
+   *   this.addAtHead(T data) ... void
+   *   this.addAtTail(T data) ... void
+   *   this.removeFromHead() ... T
+   *   this.removeFromTail() ... T
+   *   this.find(Predicate<T>) ... ANode<T>
+   *   this.removeNode(ANode<T>) ... void
    * methods for fields: 
    * 
    */
+  
+  int size() {
+    return 0;
+  }
+  
+  void addAtHead(T data) {
+    this.header.next = new Node<T>(data, this.header.next, this.header);
+  }
+  
+  void addAtTail(T data) {
+    this.header.prev = new Node<T>(data, this.header, this.header.prev);
+  }
+  
+  T removeFromHead() {
+    return null;
+  }
+  
+  T removeFromTail() {
+    return null;
+  }
+  
+  ANode<T> find(Predicate<T> pred) {
+    return null;
+  }
+  
+  void removeFromList(ANode<T> node) {
+    
+  }
   
 }
 
@@ -69,7 +106,7 @@ class Node<T> extends ANode<T>{
   
   // takes two nodes and a T value
   // changes the previous nodes of the given nodes
-  Node(ANode<T> next, ANode<T> prev, T data) {
+  Node(T data, ANode<T> next, ANode<T> prev) {
     this.next = next;
     if (next == null) {
       throw new IllegalArgumentException("Cannot take a null ANode");
@@ -101,21 +138,57 @@ class ExamplesDeque {
   
   // example variables
   Deque<String> deque1 = new Deque<String>();
-  
-  Deque<String> deque2 = new Deque<String>();
+  Deque<String> dequeAddAtHead = new Deque<String>();
+  Sentinel<String> sent1 = new Sentinel<String>();
   ANode<String> node1 = new Node<String>("abc");
   ANode<String> node2 = new Node<String>("bcd");
   ANode<String> node3 = new Node<String>("cde");
   ANode<String> node4 = new Node<String>("def");
+  ANode<String> nodeAddAtHead = new Node<String>("hi");
   
   
-  Deque<String> deque3 = new Deque<String>();
+  Sentinel<String> sent2 = new Sentinel<String>();
   ANode<String> node5 = new Node<String>("the");
   ANode<String> node6 = new Node<String>("internets");
   ANode<String> node7 = new Node<String>("busiest");
   ANode<String> node8 = new Node<String>("music");
   ANode<String> node9 = new Node<String>("nerd");
+  Deque<String> deque2 = new Deque<String>();
   
+  void initData() {
+    deque1 = new Deque<String>();
+    sent1 = this.deque1.header;
+    node1 = new Node<String>("abc", this.sent1, this.sent1);
+    node2 = new Node<String>("bcd", this.sent1, this.node1);
+    node3 = new Node<String>("cde", this.sent1, this.node2);
+    node4 = new Node<String>("def", this.sent1, this.node3);
+    nodeAddAtHead = new Node<String>("hi", this.sent1, this.node4);
+    
+    sent2 = new Sentinel<String>();
+    node5 = new Node<String>("the", this.sent2, this.sent2);
+    node6 = new Node<String>("internets", this.sent2, this.node5);
+    node7 = new Node<String>("busiest", this.sent2, this.node6);
+    node8 = new Node<String>("music", this.sent2, this.node7);
+    node9 = new Node<String>("nerd", this.sent2, this.node8);
+    deque2 = new Deque<String>(this.sent2);
+  }
+  
+  void testDeque(Tester t) {
+    this.initData();
+    t.checkExpect(this.node2.prev, this.node1);
+    t.checkExpect(this.node2.next, this.node3);
+    t.checkExpect(this.node4.next, this.deque1.header);
+    t.checkExpect(this.deque2.header, this.sent2);
+    t.checkExpect(this.deque2.header.next, this.node5);
+  }
+  
+  void testAddAtHead(Tester t) {
+    this.initData();
+    t.checkExpect(this.deque1.header.next, node1);
+    this.deque1.addAtHead("hi");
+    t.checkExpect(this.deque1, )
+    
+  }
   
 }
 
